cabe0db841a4a2074ca0bd5585a0fc2d264ba3c4
jsk900 <jsk900@yahoo.co.uk>
2020-02-03 20:16:02 +0100

    cc

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.post('/upload', (req, res) => {
 
   const file = req.files.file;
 
-  file.mv(`/../../uploads/${file.name}`, err => {
+  file.mv(`${__dirname}/../../${file.name}`, err => {
     if (err) {
       console.error(err);
       return res.status(500).send(err);
