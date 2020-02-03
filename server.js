7ffc4bb6d141084617577ad50d7c891e181290e2
jsk900 <jsk900@yahoo.co.uk>
2020-02-03 15:57:46 +0100

    uploader

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const fileUpload = require('express-fileupload');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(fileUpload());
+
+//Upload Endpoint
+app.post('/upload', (req, res) => {
+  if (req.files.file === null) {
+    return res.status(400).json({ msg: 'No file was uploaded' });
+  }
+
+  const file = req.files.file;
+
+  file.mv(`${__dirname}/client/public/uploads/${file.name}`, err => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send(err);
+    }
+
+    res.json({ fileName: file.name, filePath: `/uploads/${file.name}` });
+  });
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
