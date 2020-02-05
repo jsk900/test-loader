29b63d0c0b37a652f8250d1f8c04cfafc606dac6
jsk900 <jsk900@yahoo.co.uk>
2020-02-05 08:51:52 +0100

    cc

--- server.js
+++ server.js
@@ -15,17 +15,20 @@ app.post('/upload', (req, res) => {
 
   const file = req.files.file;
 
-  file.mv(`https://weather.goldencat.co.uk/uploads/${file.name}`, err => {
-    if (err) {
-      console.error(err);
-      return res.status(500).send(err);
-    }
+  file.mv(
+    `/home/u754198572/domains/goldencat.co.uk/public_html/uploader/uploads ${file.name}`,
+    err => {
+      if (err) {
+        console.error(err);
+        return res.status(500).send(err);
+      }
 
-    res.json({
-      fileName: file.name,
-      filePath: `http://uploader.goldencat.co.uk/${__dirname}/public_html/uploader/uploads/${file.name}`
-    });
-  });
+      res.json({
+        fileName: file.name,
+        filePath: `http://uploader.goldencat.co.uk/${__dirname}/public_html/uploader/uploads/${file.name}`
+      });
+    }
+  );
 });
 
 const PORT = process.env.PORT || 5000;
